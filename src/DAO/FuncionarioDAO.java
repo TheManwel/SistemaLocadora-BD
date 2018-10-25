533dd6108cca662839f053510d358e7765fcd74e
TheManwel <peduelsinho@gmail.com>
2018-10-25 19:13:18 -0300

    [definicao das classes de modelo]

--- src/DAO/FuncionarioDAO.java
+++ src/DAO/FuncionarioDAO.java
@@ -1,4 +1,30 @@
 package DAO;
-public class FuncionarioDAO {
-    
+
+import java.sql.*;
+import modelo.Funcionario;
+
+public class FuncionarioDAO extends ExeculteSQL {
+    public FuncionarioDAO(Connection con){
+        super(con);
+    }
+    public boolean Logar(String login, String senha){
+        boolean finalResult = false;
+        try{
+            String consulta = "select login, senha from funcionario" + "wherw login = '"+login+"'and seha = '"+senha+"'";
+            PreparedStatement ps = getCon().prepareStatement(consulta);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs != null){
+                while (rs.next()){
+                    Funcionario a = new Funcionario();
+                    a.setLogin(rs.getString(1));
+                    a.setSenha(rs.getString(2));
+                    finalResult = true;
+                }
+        }
+        }catch (SQLException ex){
+            ex.getMessage();
+        }
+        return finalResult;
+    }
 }
